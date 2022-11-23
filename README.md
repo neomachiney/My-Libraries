b5b6826517c6887d8d6d108051e6623692417635
Neomachiney <neomachine@proton.me>
2022-11-23 19:18:54 +0545

    07:18:54 PM 11/23/2022

---
+++ README.md
@@ -0,0 +1,5 @@
+# My-Libraries
+## Library List
+* **[Go Basic](https://github.com/neomachiney/GoBasic)** : Go Functions
+* **[ConfigurableShell](https://github.com/neomachiney/ConfigurableShell)** : Build shell for RCE
+
---
+++ setup.sh
@@ -0,0 +1,15 @@
+shopt -s expand_aliases
+source ~/.bashrc
+source ~/.bash_aliases
+source ~/.bash_profile
+passmepass="$GITHUB_ACCESS_TOKEN_COMMANDLINE"
+github_password=":""$passmepass""@"
+
+# Clone repositories
+cat README.md |  awk -F '(' '{print $2}' | awk -F ')**' '{print $1}' | sort -u | while read repository
+do
+	if [ ! -z "$repository" ]; then
+		git clone `echo "$repository"".git" | sed s/'github.com'/'neomachiney:@github.com'/g | sed s/':@'/$github_password/g`
+	fi
+done
+gitpass
